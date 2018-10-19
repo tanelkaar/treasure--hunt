f4d9e09f2d200484af8248b91f48770ab8485158
Lauri Lättemäe <lauri.Lattemae@nortal.com>
2018-10-19 21:31:18 +0300

    map fixes

--- src/main/webapp/js/ui/map.ctrl.js
+++ src/main/webapp/js/ui/map.ctrl.js
@@ -1,81 +1,84 @@
-(function() {
-	'use strict'
+(function () {
+    'use strict'
 
-	angular
-		.module('treasurehunt.ui')
-		.controller('MapCtrl', MapCtrl);
-	
-	function MapCtrl($state, $interval, NgMap) {
-		let refreshMapWatcher;
-		let locationWatcher;
-		let ctrl = {
-			currentPos: {},
-			map: {},
-			markers: []
-		};
-		init();
-		return ctrl = angular.extend(this, ctrl);
+    angular
+        .module('treasurehunt.ui')
+        .controller('MapCtrl', MapCtrl);
 
-		function init() {
-			initPos();
-			refreshMapWatcher = $interval(refreshMap, 5000);
-		}
-		
-		function initPos() {
-			console.log('init pos');
-			locationWatcher = navigator.geolocation.watchPosition(function(pos) {
-				console.log('up pos: ', pos);
-				ctrl.currentPos = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-				if (_.isEmpty(ctrl.markers)) {
-					for (let i = 0; i <= 5; i++) {
-						ctrl.markers.push({
-							id: 'marker: ' + i,
-							pos: [ getRandomPos(ctrl.currentPos.lat()), getRandomPos(ctrl.currentPos.lng())],
-							visible: true
-						});
-					}
-					console.log('markers: ', ctrl.markers);
-				}
-			},
-			function(error) {
-				console.error('error getting pos');
-			}, {
-				enableHighAccuracy: true,
-				timeout: 5000,
-				maximumAge: 0
-			});
-		}
-		
-		function getRandomPos(pos) {
-			return pos + ((parseInt(Math.random() * 100) % 2 == 0 ? -1 : 1) * (Math.random() / 500))
-		}
+    function MapCtrl($state, $interval, NgMap) {
+        let refreshMapWatcher;
+        let locationWatcher;
+        let ctrl = {
+            currentPos: null,
+            map: null,
+            markers: []
+        };
+        init();
+        return ctrl = angular.extend(this, ctrl);
 
-		function refreshMap() {
-			NgMap.getMap().then(function(map) {
-				ctrl.map = map;
-				if (!ctrl.currentPos) {
-					console.log('no position');
-					return;
-				}
+        function init() {
+            initPos();
+            refreshMapWatcher = $interval(refreshMap, 5000);
+        }
 
-				console.log('up shapes');
-				_.each(map.shapes, (shape) => {
-					if (shape.getBounds().contains(ctrl.currentPos)) {
-						console.log('start challenge');
-						let marker = _.find(ctrl.markers, (marker) => {
-							return marker.id === shape.id;
-						});
-						marker.visible = false;
-						cleanup();
-						$state.go('challenge', { id: marker.id });
-					}
-				});
-			});
-		}
-		
-		function cleanup() {
-			navigator.geolocation.clearWatch(locationWatcher);
-			$interval.cancel(refreshMapWatcher);
-		}
-	}
+        function initPos() {
+            console.log('init pos');
+            locationWatcher = navigator.geolocation.watchPosition(function (pos) {
+                console.log('up pos: ', pos);
+                ctrl.currentPos = {
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                };
+                if (_.isEmpty(ctrl.markers)) {
+                    for (let i = 0; i <= 5; i++) {
+                        ctrl.markers.push({
+                            id: 'marker: ' + i,
+                            pos: [getRandomPos(ctrl.currentPos.lat), getRandomPos(ctrl.currentPos.lng)],
+                            visible: true
+                        });
+                    }
+                    console.log('markers: ', ctrl.markers);
+                }
+            }, function (error) {
+                console.error('error getting pos');
+            }, {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            });
+        }
+
+        function getRandomPos(pos) {
+            return pos + ((parseInt(Math.random() * 100) % 2 == 0 ? -1 : 1) * (Math.random() / 500))
+        }
+
+        function refreshMap() {
+            NgMap.getMap().then(function (map) {
+                ctrl.map = map;
+                if (!ctrl.currentPos) {
+                    console.log('no position');
+                    return;
+                }
+
+                console.log('up shapes');
+                let pos = new google.maps.LatLng(ctrl.currentPos.lat, ctrl.currentPos.lng);
+                _.each(map.shapes, (shape) => {
+                    if (shape.getBounds().contains(pos)) {
+                        console.log('start challenge');
+                        let marker = _.find(ctrl.markers, (marker) => {
+                            return marker.id === shape.id;
+                        });
+                        marker.visible = false;
+                        cleanup();
+                        $state.go('challenge', { id: marker.id });
+                    }
+                });
+            });
+        }
+
+        function cleanup() {
+            navigator.geolocation.clearWatch(locationWatcher);
+            $interval.cancel(refreshMapWatcher);
+        }
+    }
 })();
